8778bda641219a109ea4665b768761a21558f009
Hyde Ennui <hydeennui@gmail.com>
2024-08-27 00:34:19 -0700

    Update URL

---
+++ update-calendar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o basic.ics https://calendar.google.com/calendar/ical/6df9fe259f4cb910df76fec5e2513231a6f5344b8b0ced9aa02b38defb038b13%40group.calendar.google.com/public/basic.ics
