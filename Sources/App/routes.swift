9a000284cdb9c0911b7ab04aae18a349f835a69b
Hyde Ennui <hydeennui@gmail.com>
2024-08-29 23:52:48 -0700

    Updates

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,12 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    let calendarURL = URL(string: "https://calendar.google.com/calendar/ical/6df9fe259f4cb910df76fec5e2513231a6f5344b8b0ced9aa02b38defb038b13%40group.calendar.google.com/public/basic.ics")!
+    
+    app.get("calendar") { req async throws -> Response in
+        let data = try Data(contentsOf: calendarURL)
+        let tempURL = URL(fileURLWithPath: NSTemporaryDirectory().appending(UUID().uuidString + ".ics"))
+        try data.write(to: tempURL)
+        return try await req.fileio.asyncStreamFile(at: tempURL.path())
     }
 }
